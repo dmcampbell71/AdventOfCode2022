072604d3036d0ef33a7c9731a027bef1a469b26e
DaveC <dave.campbell@innovateauto.com>
2022-12-05 10:58:46 -0600

    Completed Day 3 challenge

--- Day03/main.py
+++ Day03/main.py
@@ -18,19 +18,43 @@ def enumerate() -> dict:
     return priority_score
 
 
-def main(inventories: list):
-    dbg_cnt = 0
-    scoring = enumerate()
+def part1(inventories: list):
     sum = 0
     for inventory in inventories:
-        dbg_cnt += 1
         x = int(len(inventory) / 2)
         first = set(inventory[:x])
         second = set(inventory[x:])
         for char in first:
             if char in second:
                 sum += scoring[char]
-    print(f"Total score sum is {sum}")
+    print(f"Part 1 Total score sum is {sum}")
+
+
+def part2(inventories: list):
+    sum = 0
+    dbg_cnt = 0
+    while inventories:
+        dbg_cnt += 1
+        group = inventories[:3]
+        del inventories[:3]
+        first_elf = set(group[0].rstrip())
+        second_elf = set(group[1].rstrip())
+        third_elf = set(group[2].rstrip())
+        char_cnt=0
+        for char in first_elf:
+            char_cnt += 1
+            if char in second_elf and char in third_elf:
+                sum += scoring[char]
+                break
+    print(f"Part 2 Total score sum in {sum}")
+
+
+def main(inventories: list):
+    global scoring
+    scoring = enumerate()
+    part1(inventories)
+    part2(inventories)
+    pass
 
 
 if __name__ == '__main__':
